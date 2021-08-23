16690fca672390535900712f6963746ce2c96d5e
gavin33361 <gzheng0214@gmail.com>
2021-08-23 04:11:35 -0400

    Added a constants file to store the links

---
+++ src/constants/links.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "projects",
+    url: "/projects",
+  },
+  {
+    id: 3,
+    text: "contact",
+    url: "/contact",
+  },
+  {
+    id: 4,
+    text: "about me",
+    url: "/about",
+  },
+];
+
+export default data;
