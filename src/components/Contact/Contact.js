a82a45bb553256ea6489d82adc92722efe20b912
gavin33361 <gzheng0214@gmail.com>
2021-08-25 03:41:40 -0400

    Worked on contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+import * as styles from "./contact.module.css";
+import LinkButton from "../Button/LinkButton";
+
+const Contact = () => {
+  return (
+    <section className={styles.contact}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Want To Get In Touch?</h1>
+        <div className={styles.line}></div>
+        <div className={styles.info}>
+          <p>
+            Feel free to reach out if you have any questions or just want to
+            have a chat!
+          </p>
+          <LinkButton
+            text="Send me an email"
+            url="mailto:gzheng0214@gmail.com"
+            email
+            dark
+            hoverDark
+            arrow
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
