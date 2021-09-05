e15ac4049b81fc057e326b8aa21e1a20648f223b
gzheng0214 <gzheng0214@gmail.com>
2021-09-04 22:42:53 -0400

    Added constant files

---
+++ src/constants/experiences.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    title: "UI Engineer Intern",
+    workplace: "Monster Worldwide Inc.",
+    timespan: "June 2021 - Aug. 2021",
+    description: [
+      "Worked on building admin dashboards and wiring the UI with CRUD functionalities from the backend",
+      "Collaborated with other interns to create a web application that is used by developers to track changes in the Jobsdeployment pipeline",
+      "Created endpoints in Spring Boot to read and upload files from/to AWS S3",
+      "Refactored Java code for more efficient execution and easier maintainability",
+      "Wrote unit tests in Jest and JUnit to ensure quality and functionality of code",
+    ],
+  },
+  {
+    title: "Media Services Assistant",
+    workplace: "Fordham University",
+    timespan: "Sep. 2018 – May 2019",
+    description: [
+      "Maintain Fordham’s campus technology, including classroom computers, projectors, and video conferencing",
+      "Provide portable technology to rooms without A/V equipment for professors, university administrators, and third-party clients",
+      "Set up A/V equipment for events and classroom recordings",
+      "Answer incoming tech support calls and aid faculty members",
+    ],
+  },
+];
+
+export default data;
---
+++ src/constants/social_links.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
+
+const data = [
+  {
+    icon: <AiFillGithub />,
+    url: "https://github.com/gzheng0214",
+    target: true,
+  },
+  {
+    icon: <AiFillLinkedin />,
+    url: "https://www.linkedin.com/in/gzheng0214/",
+    target: true,
+  },
+  {
+    icon: <AiOutlineMail />,
+    url: "mailto:gzheng0214@gmail.com",
+  },
+];
+
+export default data;
